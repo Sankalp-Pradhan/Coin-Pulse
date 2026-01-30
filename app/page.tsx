0a22ba47713e2a4a8d694ac280ed7ed709e575ef
Sankalp Pradhan <sankalppradhan1906@gmail.com>
2026-01-30 16:06:18 +0530

    implement coin overview and  trending coins ui and functionality using the fetcher utility function

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,78 @@
-import { Button } from "@/components/ui/button"
+import Image from "next/image"
+import Link from "next/link";
+import { cn } from "@/lib/utils";
+import { TrendingDown, TrendingUp } from "lucide-react";
+import { Suspense } from "react";
+import CoinOverview from "@/components/home/CoinOverview";
+import TrendingCoins from "@/components/home/TrendingCoins";
+import { CoinOverviewFallback, TrendingCoinsFallback } from "@/components/home/Fallback";
 
-export default function Home() {
+const columns: DataTableColumn<TrendingCoin>[] = [
+  {
+    header: 'Price', cellClassName: 'price-cell', cell: (coin) => coin.item.data.price
+  },
+
+  {
+    header: 'Name',
+    cellClassName: 'name-cell',
+    cell: (coin) => {
+      const item = coin.item;
+
+      return (
+        <Link href={`/coins/${item.id}`}>
+          <Image src={item.large} alt={item.name} width={36} />
+          <p>{item.name}</p>
+        </Link>
+      )
+    }
+  },
+
+  {
+    header: "24h Change",
+    cellClassName: 'name-cell',
+    cell: (coin) => {
+      const item = coin.item;
+      const isTrendingUp = item.data.price_change_percentage_24h.usd > 0;
+
+      return (
+        <div className={cn('price-change', isTrendingUp ? 'text-green-500' : 'text-red-500')}>
+          <p>
+            {isTrendingUp ? (
+              <TrendingUp width={16} height={16} />
+            ) : (
+              <TrendingDown width={16} height={16} />
+            )}
+          </p>
+        </div>
+      )
+
+    }
+  }
+]
+
+
+
+export default async function Home() {
+
   return (
-    <div>
-      Main
-    </div>
+    <main className="main-container">
+      <section className="home-grid ">
+        <Suspense fallback={<CoinOverviewFallback />}>
+          <CoinOverview />
+        </Suspense>
+
+        <Suspense fallback={<TrendingCoinsFallback />}>
+          <TrendingCoins />
+        </Suspense>
+      </section>
+
+
+      <section className="w-full mt-7 space-y-4 ">
+        <p>Categories</p>
+      </section>
+
+
+
+    </main>
   )
 }
